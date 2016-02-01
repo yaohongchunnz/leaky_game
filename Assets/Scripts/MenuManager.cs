ebf8b5b70e225fbd19575e3a999f1c5c82e9ee68
Yao Hong Chun <yhchun1992@hotmail.com>
2016-02-02 00:08:31 +1300

    End of Day commit!
    basic multiplayer
    some terrain assets added

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void HostGame()
+    {
+        
+    }
+    public void JoinGame()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkPlayer : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            GetComponentInChildren<AudioListener>().enabled = true;
+            GetComponentInChildren<GUILayer>().enabled = true;
+            GetComponentInChildren<FlareLayer>().enabled = true;
+        }
+    }
+}
